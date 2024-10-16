0eb3d00bf1630bf8109ff309681b9a685d8e6046
Muhammad Muzammil Siddiqui <142095821+MuxammilSidd@users.noreply.github.com>
2024-10-16 11:37:57 +0500

    update

--- DSALabMid.cpp
+++ DSALabMid.cpp
@@ -1088,6 +1088,37 @@ void shellSort(int myarr[], int n1) {
     }
 }
 
+int binSearchAsc(int *arr,int n,int key){      //When sorted ascending
+    int left = 0;
+    int right = n-1;
+    while(left <= right) {
+        int mid = left + (right - left)/2;
+
+        if (arr[mid] == key) 
+            return mid;
+        else if (arr[mid] < key) 
+            left = mid + 1;
+        else 
+            right = mid - 1;
+    }
+    return -1;
+}
+int binSearchDesc(int *arr,int n,int key){      //When sorted descending
+    int left = 0;
+    int right = n-1;
+    while(left <= right) {
+        int mid = left + (right - left)/2;
+
+        if (arr[mid] == key) 
+            return mid;
+        else if (arr[mid] > key) 
+            left = mid + 1;
+        else 
+            right = mid - 1;
+    }
+    return -1;
+}
+
 //-----------------------------------------------------------------------------------------------------------//
 
 int main() {
